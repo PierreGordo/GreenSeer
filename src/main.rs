56a8ab4855b28a4c3d7f9701d7040106c9004b4a
PierreGordo <peter.gardas@gmail.com>
2026-03-30 08:50:10 +0200

    Had to go

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use axum::extract::State;
+use axum::http::StatusCode;
 use axum::{response::Html, routing::get, Router};
+use minijinja::{context, Enviroment};
+use std::sync::Arc;
+
 
 //async
 #[tokio::main]
